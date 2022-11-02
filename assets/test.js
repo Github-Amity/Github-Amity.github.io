531e335b7511733db7f1efd4bddcd17bb8c0b182
Tarctic <zainabshah2003@gmail.com>
2022-11-02 14:53:44 +0300

    test done

--- assets/test.js
+++ assets/test.js
@@ -4,14 +4,29 @@ function() {
 })
 
 function loadData() {
+
+    const projects = [
+        "Members contributions",    // 0
+        "Java Objects",             // 1
+        "Java Classes",             // 2
+    ];
+
     const tarctic = {
         score: 1500,
         rank: 1,
-        projects: [
-            ""
-        ]
+        projects: projects[0,3]
     }
 
 	const score = document.querySelector('#score');
     score.innerHTML = tarctic.score;
 }
+
+/*
+
+- change variable names and file name
+- create div element (ordered list) for projects
+- create function to calculate rank
+- create others info objects
+- make a proper score system
+
+*/
