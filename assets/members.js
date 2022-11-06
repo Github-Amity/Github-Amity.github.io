caddac7d889d0d3abc1f070fb26fea47676f8be7
Tarctic <zainabshah2003@gmail.com>
2022-11-06 21:50:47 +0300

    Add new cards, other changes

--- assets/members.js
+++ assets/members.js
@@ -17,54 +17,63 @@ function loadData() {
             score: 1500,
             rank: 1,
             projects: projects.slice(0,2),
+            img: "maryam.jpg",
         },
         {
             username: "Bhavya",
             score: 1500,
             rank: 1,
             projects: projects.slice(0,2),
+            img: "bhavya.jpg",
         },
         {
             username: "Tarctic",
             score: 1500,
             rank: 1,
             projects: projects.slice(0,3),
+            img: "tarctic.jpg",
         },
         {
             username: "Rahul",
             score: 1000,
             rank: 2,
             projects: projects.slice(1,2),
+            img: "rahul.jpg",
         },
         {
             username: "Sohan",
             score: 500,
             rank: 3,
             projects: projects.slice(0,1),
+            img: "nope.jpg",
         },
         {
             username: "Sourabh",
             score: 0,
             rank: 4,
             projects: projects.slice(0,3),
+            img: "nope.jpg",
         },
         {
             username: "Puni",
             score: 0,
             rank: 4,
             projects: projects.slice(0,3),
+            img: "nope.jpg",
         },
         {
             username: "Hannan",
             score: 0,
             rank: 4,
             projects: projects.slice(),
+            img: "hannan.jpg",
         },
         {
             username: "Sher",
-            score: 1500,
-            rank: 1,
+            score: 0,
+            rank: 4,
             projects: projects.slice(),
+            img: "nope.jpg",
         },
     ]
 
@@ -72,6 +81,10 @@ function loadData() {
     members.forEach(function(member) {
         console.log(member)
         if (member.username == username) {
+
+            const img = document.querySelector('img');
+            img.src = member.img;
+
             const username = document.getElementById('username');
             username.innerHTML = member.username;
             const score = document.getElementById('score');
@@ -98,5 +111,6 @@ DONE - create div element (ordered list) for projects
 - create function to calculate rank
 DONE - create others info objects
 - make a proper score system
+- use hide and block and one single html file for the whole website
 
 */
